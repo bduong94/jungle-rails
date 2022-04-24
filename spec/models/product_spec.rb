f59dcecf463fb87c6f5e6656726c7dc879ea1d4a
Brian Duong <brianminhduong@gmail.com>
2022-04-24 03:00:39 +0000

    Set up tests for Product

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+require "Product"
+
+describe "Tests for Products" do
+  describe "Validations" do
+
+    it "Contains a name, price, quantity and category" do
+    end
+
+    it "Does not contain a name" do
+    end
+
+    it "Does not contain a price" do
+    end
+
+    it "Does not contain a quantity" do
+    end
+
+    it "Does not contain a category" do
+    end
+  end
+end

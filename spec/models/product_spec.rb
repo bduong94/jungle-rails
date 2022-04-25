de62a2d6a859e7b0b28c2fff17b1ea550e28e395
Brian Duong <brianminhduong@gmail.com>
2022-04-25 03:43:57 +0000

    Create test for does not contain name

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -25,6 +25,11 @@ describe "Tests for Products" do
     end
 
     it "Does not contain a name" do
+      sample_product = Product.new({name: nil, price_cents: 30000, quantity: 30, category_id: 2})
+      expect(sample_product.name).to be_nil
+      expect(sample_product.price_cents).to_not be_nil
+      expect(sample_product.quantity).to_not be_nil
+      expect(sample_product.category_id).to_not be_nil
     end
 
     it "Does not contain a price" do

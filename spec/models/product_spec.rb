0c6e1f12295f508d11b5ffdd13b5701c71fcc1a5
Brian Duong <brianminhduong@gmail.com>
2022-04-25 04:21:02 +0000

    Add category to testing file

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 require "Product"
+require "Category"
 
 # Refrence
 # create_table "products", force: :cascade do |t|
@@ -15,21 +16,20 @@ require "Product"
 
 describe "Tests for Products" do
   describe "Validations" do
+    before do
+      @sample_category = Category.new({name:"Furniture"})
+      @sample_product = Product.new({name: "Table", price_cents: 30000, quantity: 30, category: @sample_category})
+    end
 
     it "Contains a name, price, quantity and category" do
-      sample_product = Product.new({name: "Table", price_cents: 30000, quantity: 30, category_id: 2})
-      expect(sample_product.name).to_not be_nil
-      expect(sample_product.price_cents).to_not be_nil
-      expect(sample_product.quantity).to_not be_nil
-      expect(sample_product.category_id).to_not be_nil
+      expect(@sample_product).to be_valid
     end
 
     it "Does not contain a name" do
-      sample_product = Product.new({name: nil, price_cents: 30000, quantity: 30, category_id: 2})
-      expect(sample_product.name).to be_nil
-      expect(sample_product.price_cents).to_not be_nil
-      expect(sample_product.quantity).to_not be_nil
-      expect(sample_product.category_id).to_not be_nil
+      # expect(sample_product.name).to be_nil
+      # expect(sample_product.price_cents).to_not be_nil
+      # expect(sample_product.quantity).to_not be_nil
+      # expect(sample_product.category_id).to_not be_nil
     end
 
     it "Does not contain a price" do

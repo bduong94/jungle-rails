f19d2a44a0d98853b199119d211a45a3dadd7671
Brian Duong <brianminhduong@gmail.com>
2022-04-26 05:00:15 +0000

    Add validations for empty information

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ActiveRecord::Base
     has_secure_password
     
     validates :first_name, presence: true
-    # validates :last_name, presence: true
-    # validates :email, presence: true
-    # validates :password, presence: true
-    # validates :password_confirmation, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true
+    validates :password, presence: true
+    validates :password_confirmation, presence: true
 
     def authenticate_with_credentials email, password
         # user = User.find_by_email(params[:email])

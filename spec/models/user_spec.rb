d29af745e596e29fa3abda8e8a718c5b4ba7a32e
Brian Duong <brianminhduong@gmail.com>
2022-04-26 04:58:44 +0000

    Create tests for blank information

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,8 +10,51 @@ RSpec.describe User, type: :model do
   #   t.datetime "created_at",      null: false
   #   t.datetime "updated_at",      null: false
   # end
-  
+
   describe 'Validations' do
+    before do
+      @first_user = User.new({first_name: "Tom", last_name:"Dinkle", email: "test123@test.com", password:"Fast", password_confirmation:"Fast"})
+    end
+
+    it 'Creates the new user' do
+      expect(@first_user).to be_valid
+    end
+
+    it 'does not have a blank first_name' do
+      @first_user.first_name = nil
+      expect(@first_user).to_not be_valid
+      expect(@first_user.errors.full_messages).to include("First name can't be blank")
+    end
+
+    it 'does not have a blank last_name' do
+      @first_user.last_name = nil
+      expect(@first_user).to_not be_valid
+      expect(@first_user.errors.full_messages).to include("Last name can't be blank")
+    end
+
+    it 'does not have a blank email' do
+      @first_user.email = nil
+      expect(@first_user).to_not be_valid
+      expect(@first_user.errors.full_messages).to include("Email can't be blank")
+    end
+
+    it 'does not have a blank password' do
+      @first_user.password = nil
+      expect(@first_user).to_not be_valid
+      expect(@first_user.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it 'does not have a blank password confirmation' do
+      @first_user.password_confirmation = nil
+      expect(@first_user).to_not be_valid
+      expect(@first_user.errors.full_messages).to include("Password confirmation can't be blank")
+    end
+
+    it 'does not have matching password and password confirmation' do
+    end
+
+    it 'does not use a unique email' do
+    end
   end
 
   describe '.authenticate_with_credentials' do

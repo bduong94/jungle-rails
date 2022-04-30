915db65399c369745081acaeb2355a5dae7500f2
Brian Duong <brianminhduong@gmail.com>
2022-04-30 03:28:09 +0000

    Set up database, debug and verify for testing feature

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js:true do
+ 
+  #SET UP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario 'User visits home page and adds item to cart' do
+    #ACT
+    
+    #DEBUG
+    save_screenshot
+
+    #VERIFY
+    expect(find('nav')).to have_content("Cart (1)")
+  end
+end
